089f51dfe4de1a117926b986f112ae0c2ea38bb4
Florian Blaschke <florianblaschke@web.de>
2023-10-14 00:37:39 +0200

    added middleware

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { User } from "next-auth";
+import { signOut } from "next-auth/react";
+import Link from "next/link";
+import UserAvatar from "./UserAvatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -8,9 +11,6 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/DropdownMenu";
-import UserAvatar from "./UserAvatar";
-import Link from "next/link";
-import { signOut } from "next-auth/react";
 
 interface UserAccountNav {
   user: Pick<User, "name" | "image" | "email">;
